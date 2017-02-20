e4cb0c849cd2cceaae2a73c5af672ef155a8761a
Irene <carina.omsk@gmail.com>
2017-02-20 05:03:31 +0000

    Added creation and deleting reviews only for logged in users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,19 @@
 class ReviewsController < ApplicationController
+
+  before_filter :authorize
+
   def create
     @product = Product.find params[:product_id]
     @review = @product.reviews.create(review_params)
     @review.user = current_user
     @review.save
+    redirect_to :back
+  end
+
+  def destroy
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
     redirect_to :back
   end
 
