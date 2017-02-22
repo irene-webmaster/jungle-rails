d849b5881046d838f282275e0860654763de4fd3
Irene <carina.omsk@gmail.com>
2017-02-22 00:00:03 +0000

    Clenup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate_with_credentials(params[:email], params[:password])
-
     if user
       session[:user_id] = user.id
       redirect_to '/'
